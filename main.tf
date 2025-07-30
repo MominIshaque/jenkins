cf425a624c45dfbee92a10c4acf7ea42f5ac09d9
Momin Ishaque <168473342+MominIshaque@users.noreply.github.com>
2025-07-30 15:27:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "dev" {
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t3.medium"
     tags = {
-      Name = "hi from jenkinssss"
+      Name = "hi this is momin"
     }
 }
